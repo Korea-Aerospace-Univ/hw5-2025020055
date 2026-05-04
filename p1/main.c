3d9684bcfd663e6193dcd7d733c6c12929e453a7
2025020055 <kimsua005@gmail.com>
2026-05-04 15:27:56 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    int N=0, a=1, b=2, c=1;
+    int N;
     scanf("%d", &N);
     
     bool found=false;
     
-    for (a=1; a<=(N/900); a+=1){
-        for (b=2; b<=(N/750); b+=2){
-            for(c=1; c<=(N/200); c+=1){
+    for (int a=1; a<=(N/900); a+=1){
+        for (int b=2; b<=(N/750); b+=2){
+            for(int c=1; c<=(N/200); c+=1){
                 if (c<a||c<b) {
                     if (900*a+750*b+200*c==N){
                         printf("%d %d %d\n", a, b, c);
